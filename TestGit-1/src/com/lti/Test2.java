9c579e96b196240ac2c4d98ec059a5820aa1d5d6
HP <abnv.agr@gmail.com>
2020-09-10 15:36:41 +0530

    2nd Commit

--- TestGit-1/src/com/lti/Test2.java
+++ TestGit-1/src/com/lti/Test2.java
@@ -1,10 +1,10 @@
 package com.lti;
 
-public class Test {
+public class Test2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Test2");
+		System.out.println("Test.....");
 	}
 	
 }
